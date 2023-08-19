dbabed679e8532345139603ef71dd4cbe8fd923f
JRRN <jrrn@JRRN-MB.local>
2023-08-19 14:27:07 +0200

    Added NextUI2

--- Modulo5/tictactoe/tailwind.config.js
+++ Modulo5/tictactoe/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 const defaultTheme = require('tailwindcss/defaultTheme')
+const {nextui} = require("@nextui-org/react");
 
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {
@@ -13,6 +16,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 
