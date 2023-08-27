e96c98c428aa01d30af8fe4d4ee2433609940efd
JRRN <jrrn@JRRN-MB.local>
2023-08-27 13:03:43 +0200

    Modulo 8

---
+++ Modulo6/ejemplos_simples_rutas/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {BrowserRouter} from "react-router-dom";
+import LandingRutas2 from "./LandingRutas2";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <LandingRutas2 />
+  </BrowserRouter>
+);
+
---
+++ Modulo8/HelloWorld/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <Text>Hola a todos!</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
